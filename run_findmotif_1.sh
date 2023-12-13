29e1181401dcaf8c7c31510c8e834c70f0a78dc6
Xianglmoffitt <xiang.liu@moffitt.org>
2023-12-13 18:16:01 -0500

    upload

---
+++ run_findmotif_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -N findmotif_1
+#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=4,mem=12gb
+#PBS -m bea
+#PBS -M Xiang.Liu@moffitt.org
+
+export PROJECT_DIR="/share/lab_teng/xiangliu/homer_motif"
+export FASTA="$PROJECT_DIR/fasta/npc.chipseq.fa"
+export OUT_DIR="$PROJECT_DIR/npc_motif"
+
+module load homer/4.9
+
+cd $PROJECT_DIR
+
+
+~/homer/bin/findMotifs.pl $FASTA fasta $OUT_DIR
